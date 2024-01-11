e5fa2ef53ef36bd19b5644c3ea74ccc9842dc556
Nick Crisci <nick_gaetano.crisci@smail.th-koeln.de>
2024-01-11 13:50:58 +0100

    Added code to change the sensitivity depending on the distance

--- src/draft.cpp
+++ src/draft.cpp
@@ -1,4 +1,5 @@
 #include "rplidar_driver_impl.h"
+#include <math.h>
 const int lidarMotorPin = 3;
 char report[80];
 RPLidar rplidar;
@@ -36,21 +37,23 @@ class VibrationSensor {
         }
 };
 
-const int NORTH         = 1;
-const int NORTH_EAST    = 2;
-const int EAST          = 3;
-const int SOUTH_EAST    = 4;
-const int SOUTH         = 5;
-const int SOUTH_WEST    = 6;
-const int WEST          = 7;
-const int NORTH_WEST    = 8;
+const int NORTH         = 0;
+const int NORTH_EAST    = 1;
+const int EAST          = 2;
+const int SOUTH_EAST    = 3;
+const int SOUTH         = 4;
+const int SOUTH_WEST    = 5;
+const int WEST          = 6;
+const int NORTH_WEST    = 7;
 
-int vibrationSensorPins[8][2] = {{NORTH, 0}, {NORTH_EAST, 1}, {EAST, 2}, {SOUTH_EAST, 3}, {SOUTH, 4}, {SOUTH_WEST, 5}, {WEST, 6}, {NORTH_WEST, 7}};
+int vibrationSensorPins[8][2] = {{NORTH, 10}, {NORTH_EAST, 1}, {EAST, 2}, {SOUTH_EAST, 3}, {SOUTH, 4}, {SOUTH_WEST, 5}, {WEST, 6}, {NORTH_WEST, 7}};
 VibrationSensor vibrationSensors[8];
 
 void setup(){
   Serial.begin(2000000);
   pinMode(lidarMotorPin, OUTPUT);
+  //pinMode(10, OUTPUT);
+  //analogWrite(10, 255);
   setupVibrationMotors();
   rplidar.begin();
   delay(1000);
@@ -58,7 +61,7 @@ void setup(){
 
 void setupVibrationMotors() {
     for (int* sensorPin : vibrationSensorPins) {
-        vibrationSensors[sensorPin[1]] = VibrationSensor(sensorPin[1]);
+      vibrationSensors[sensorPin[0]] = VibrationSensor(sensorPin[1]);
     }
 }
 
@@ -100,8 +103,8 @@ void loop() {
       for (size_t i = 0; i < nodeCount; ++i) {
         // convert to standard units
         float angle_in_degrees = nodes[i].angle_z_q14 * 90.f / (1<<14);
-        //float distance_in_meters = nodes[i].dist_mm_q2 / 1000.f / (1<<2);
-        float distance_in_meters = nodes[i].dist_mm_q2 / (1<<2);
+        float distance_in_meters = nodes[i].dist_mm_q2 / 1000.f / (1<<2);
+        //float distance_in_meters = nodes[i].dist_mm_q2 / (1<<2);
         processData(angle_in_degrees, distance_in_meters, nodes[i].quality);
 
         //snprintf(report, sizeof(report), "%.2f:%.2f:%d", distance_in_meters, angle_in_degrees, nodes[i].quality);
@@ -112,5 +115,31 @@ void loop() {
 }
 
 void processData(float angle, float distance, int quality) {
+  int direction = getDirection(angle);
+  VibrationSensor sensor = vibrationSensors[direction];
+  int intensity = calculateIntensity(distance);
+  sensor.setIntensity(intensity);
+}
 
+int getDirection(float angle) {
+  int index = (int) round(angle / 45) % 8;
+  return index;
+}
+
+const float minDistance = 0.1;
+const float maxDistance = 1.5;
+
+int calculateIntensity(float distance) {
+  // Ensure the distance is within the valid range
+  if (distance > 1.5) {
+    return 0;
+  }
+
+  // Calculate the intensity based on the distance
+  int intensity = static_cast<int>((maxDistance - distance) / (maxDistance - minDistance) * 255.0);
+
+  // Ensure that the intensity stays within the valid range (0 to 255)
+  intensity = constrain(intensity, 0, 255);
+
+  return intensity;
 }
