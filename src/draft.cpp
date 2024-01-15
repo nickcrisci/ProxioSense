15da7340932f66f2a5c5da572b1f6e5b247a4057
Nick Crisci <nick_gaetano.crisci@smail.th-koeln.de>
2024-01-15 18:29:00 +0100

    Added strategies to sensors | Added minStrat & slidingWindowStrat

--- src/draft.cpp
+++ src/draft.cpp
@@ -6,7 +6,11 @@ RPLidar rplidar;
 
 #include <Arduino.h>
 
-const int minDistance = 10;
+const int averageStrat = 0;
+const int minStrat = 1;
+const int slidingWindowStrat = 2;
+
+const int minDistance = 300;
 const int maxDistance = 3000;
 const int bufferSize = 20;
 
@@ -16,20 +20,40 @@ private:
   int intensity;
   int distanceBuffer[bufferSize];
   int currentIndex = 0;
+  int strategy = 0;
 
   void processBuffer() {
-    float average = this->calcAverage();
-    int intensity = this->calculateIntensity(average);
-    Serial.println(intensity);
+    float distance;
+    if (this->strategy == averageStrat || this->strategy == slidingWindowStrat) {
+      distance = this->calcAverage();
+    } else if (this->strategy == minStrat) {
+      distance = this->calcMin();
+    }
+
+    int intensity = this->calculateIntensity(distance);
     this->setIntensity(intensity);
+
+    if (this->currentIndex == bufferSize) {
+      this->currentIndex = 0;
+    }
   }
 
   int calculateIntensity(float distance) {
     if (distance > maxDistance) return 0;
+    if (distance == maxDistance) return 255;
     // Calculate the intensity based on the distance
     int intensity = static_cast<int>((maxDistance - distance) / (maxDistance - minDistance) * 255.0);
-    intensity = constrain(intensity, 150, 255);
+    intensity = constrain(intensity, 0, 255);
     return intensity;
+  }
+
+  float calcMin() {
+    int minNum = 9999;
+    for (int i = 0; i < this->currentIndex; i++) {
+      if (this->distanceBuffer[i] < minDistance) continue;
+      minNum = min(minNum, this->distanceBuffer[i]);
+    }
+    return (float) minNum;
   }
 
   float calcAverage() {
@@ -48,21 +72,22 @@ private:
   }  
 
 public:
-  VibrationSensor()
-    : pin(0), intensity(0) {}  // Default constructor
+  VibrationSensor(): pin(0), intensity(0) {}  // Default constructor
 
-  VibrationSensor(int pin) {
+  VibrationSensor(int pin, int strategy) {
     this->pin = pin;
     this->intensity = 0;
+    this->strategy = strategy;
     pinMode(this->pin, OUTPUT);
   }
 
   void add(float distance) {
+    //Serial.println(this->strategy);
     this->distanceBuffer[this->currentIndex] = distance;
     this->currentIndex++;
-    if (currentIndex == bufferSize) {
+
+    if (this->strategy == slidingWindowStrat || this->currentIndex == bufferSize) {
       this->processBuffer();
-      this->currentIndex = 0;
     }
   }
 
@@ -81,6 +106,7 @@ const int WEST = 6;
 const int NORTH_WEST = 7;
 
 int vibrationSensorPins[8][2] = { { NORTH, 10 }, { NORTH_EAST, 1 }, { EAST, 2 }, { SOUTH_EAST, 3 }, { SOUTH, 4 }, { SOUTH_WEST, 5 }, { WEST, 6 }, { NORTH_WEST, 7 } };
+int sensorStrats[8] = {minStrat, averageStrat, averageStrat, averageStrat, averageStrat, averageStrat, averageStrat, averageStrat};
 VibrationSensor vibrationSensors[8];
 
 void setup() {
@@ -93,9 +119,30 @@ void setup() {
 
 void setupVibrationMotors() {
   for (int* sensorPin : vibrationSensorPins) {
-    vibrationSensors[sensorPin[0]] = VibrationSensor(sensorPin[1]);
+    int pin = sensorPin[1];
+    int strategy = sensorStrats[sensorPin[0]];
+    vibrationSensors[sensorPin[0]] = VibrationSensor(pin, strategy);
   }
 }
+
+/*void checkForCommand() {
+  if (Serial.available() >= 3) {
+    char command = Serial.read();
+    int sensor = Serial.parseInt();
+    int strategy = Serial.parseInt();
+    switch (command) {
+      case 'S':
+        Serial.print(command);
+        Serial.print(" : ");
+        Serial.print(sensor);
+        Serial.print(" : ");
+        Serial.println(strategy);
+        break;
+      default:
+        break;
+    }
+  }
+}*/
 
 void printInfo() {
   rplidar_response_device_info_t info;
@@ -119,6 +166,7 @@ void printSampleDuration() {
 //#define SCAN_TYPE_EXPRESS
 int counter = 0;
 void loop() {
+  //checkForCommand();
   if (!rplidar.isScanning()) {
     rplidar.startScanNormal(true);
     digitalWrite(lidarMotorPin, HIGH);  // turn on the motor
